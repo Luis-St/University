21bf1884548f52da0cc346fb33ef1f38da1dd290
Luis <luis@staudt-fridingen.de>
2024-12-09 23:14:18 +0100

    removed idea folder from git

--- User Interfaces/src/net/luis/ZustandsmodellInSwing.java
+++ User Interfaces/src/net/luis/ZustandsmodellInSwing.java
@@ -1,6 +1,7 @@
 package net.luis;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,7 +13,7 @@ import java.awt.event.ActionListener;
 
 public class ZustandsmodellInSwing {
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		JFrame fenster = new JFrame("Beispiel für zustandsbehaftete Benachrichtigung");
 		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fenster.setSize(400, 200);
@@ -42,5 +43,25 @@ public class ZustandsmodellInSwing {
 		
 		fenster.getContentPane().add(knopf);
 		fenster.setVisible(true);
-	}
+	}*/
+	
+	/*public static void main(String[] args) {
+		JFrame fenster = new JFrame("Synchronisierte JScrollBar und JSlider");
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenster.setSize(400, 200);
+		fenster.setLayout(new GridLayout(2, 1));
+		
+		BoundedRangeModel model = new DefaultBoundedRangeModel(50, 0, 0, 100);
+		
+		JScrollBar scrollBar = new JScrollBar(JScrollBar.HORIZONTAL);
+		scrollBar.setModel(model);
+		
+		JSlider slider = new JSlider(JSlider.HORIZONTAL);
+		slider.setModel(model);
+		
+		fenster.add(scrollBar);
+		fenster.add(slider);
+		
+		fenster.setVisible(true);
+	}*/
 }
