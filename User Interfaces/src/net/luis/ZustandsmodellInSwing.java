7937df6c661ba6ec78795e23f30f3269e85f53ca
Luis <luis@staudt-fridingen.de>
2024-12-13 10:05:16 +0100

    added tests

--- User Interfaces/src/net/luis/ZustandsmodellInSwing.java
+++ User Interfaces/src/net/luis/ZustandsmodellInSwing.java
@@ -13,39 +13,7 @@ import java.awt.event.ActionListener;
 
 public class ZustandsmodellInSwing {
 	
-	/*public static void main(String[] args) {
-		JFrame fenster = new JFrame("Beispiel für zustandsbehaftete Benachrichtigung");
-		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fenster.setSize(400, 200);
-		
-		JButton knopf = new JButton("Klicken Sie mich!");
-		knopf.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent ereignis) {
-				String befehl = ereignis.getActionCommand();
-				long zeitpunkt = ereignis.getWhen();
-				Object quelle = ereignis.getSource();
-				
-				System.out.println("Ereignisinformationen:");
-				System.out.println("Befehl: " + befehl);
-				System.out.println("Zeitpunkt: " + zeitpunkt);
-				System.out.println("Quelle: " + quelle);
-				
-				// Nachricht im Fenster anzeigen
-				JOptionPane.showMessageDialog(
-					fenster,
-					"Knopf wurde geklickt!\nBefehl: " + befehl + "\nZeitpunkt: " + zeitpunkt,
-					"Ereignisinformationen",
-					JOptionPane.INFORMATION_MESSAGE
-				);
-			}
-		});
-		
-		fenster.getContentPane().add(knopf);
-		fenster.setVisible(true);
-	}*/
-	
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		JFrame fenster = new JFrame("Synchronisierte JScrollBar und JSlider");
 		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fenster.setSize(400, 200);
@@ -53,7 +21,7 @@ public class ZustandsmodellInSwing {
 		
 		BoundedRangeModel model = new DefaultBoundedRangeModel(50, 0, 0, 100);
 		
-		JScrollBar scrollBar = new JScrollBar(JScrollBar.HORIZONTAL);
+		JScrollBar scrollBar = new JScrollBar(JScrollBar.VERTICAL);
 		scrollBar.setModel(model);
 		
 		JSlider slider = new JSlider(JSlider.HORIZONTAL);
@@ -63,5 +31,5 @@ public class ZustandsmodellInSwing {
 		fenster.add(slider);
 		
 		fenster.setVisible(true);
-	}*/
+	}
 }
