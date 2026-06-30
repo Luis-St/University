9f109cd409b2961e4d7d980e6561eb2b938829e9
Luis <luis@staudt-fridingen.de>
2026-06-30 21:33:00 +0200

    added edited tasks

---
+++ Intensive Tutorial/src/gdl/sose26/nachhilfe/Probe3Aufgabe2.java
@@ -0,0 +1,37 @@
+package gdl.sose26.nachhilfe;
+
+public class Probe3Aufgabe2 {
+	
+	/*public static class Punkt {
+		
+		public double x;
+		public double y;
+		
+		public Punkt(double x, double y) {
+			this.x = x;
+			this.y = y
+		}
+		
+		public double abstand(Punkt p) {
+			double dx = x - p.x;
+			double dy = y - p.y;
+			return Math.sqrt(dx * dx + dy * dy);
+		}
+		
+		public boolean istNah(Punkt p, double s) {
+			if (abstand(p) < s) {
+				return true;
+			} else {
+				System.out.println("zu weit");
+			}
+		}
+		
+		public static void main(String[] args) {
+			Punkt a = new Punkt(0, 0);
+			Punkt b = new Punkt(3, 4);
+			double d = a.abstand();
+			System.out.println(d)
+			boolean nah = a.istNah(b, "fuenf");
+		}
+	}*/
+}
---
+++ Intensive Tutorial/src/gdl/sose26/nachhilfe/Probe6Aufgabe2.java
@@ -0,0 +1,61 @@
+package gdl.sose26.nachhilfe;
+
+public class Probe6Aufgabe2 {
+	
+	/*public static class Geometrie {
+		
+		public double radius;
+		
+		public static double flaeche(double r) {
+			return 3.14159 * r * r
+		}
+		
+		public static double umfang(double r) {
+			return 2 * 3.14159 * r;
+		}
+		
+		public double durchmesser() {
+			return 2 * radius;
+		}
+		
+		public static boolean istGross(double r) {
+			if (r > 100) {return true;}
+		}
+	}
+	
+	public static class Rechteck {
+		
+		public int breite;
+		public int hoehe;
+		
+		public static int flaeche(int b, int h) {
+			return b * h;
+		}
+		
+		public static int skaliere(int wert, int faktor) {
+			int ergebnis = wert * faktor;
+		}
+		
+		public boolean istQuadrat(int b, int h) {
+			return b - h;
+		}
+	}
+	
+	public class Programm {
+		
+		public static void zeige(String text) {
+			System.out.println(text);
+		}
+		
+		public static void main(String[] args) {
+			double a = Geometrie.flaeche(5.0);
+			zeige(a);
+			double u = Geometrie.umfang();
+			int f = Rechteck.flaeche(4, 6);
+			boolean q = istQuadrat(4, 4);
+			System.out.println(Geometrie.radius);
+			int s = Rechteck.skaliere(3, 2, 1);
+		}
+	}*/
+	
+}
